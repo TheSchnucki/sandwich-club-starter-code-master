9363ae80e09995ba7ef9b5d08de692eb9d61c8e1
theSchnucki <theschnucki@gmail.com>
2018-02-18 19:22:12 +0100

    Parsing done Detailed design needs to be done

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -5,21 +5,31 @@ import android.util.Log;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class JsonUtils {
     private static final String TAG = JsonUtils.class.getSimpleName();
 
     public static Sandwich parseSandwichJson(String json) {
         Sandwich finalSandwich = new Sandwich();
+
+        ArrayList<String> akaList = new ArrayList<String>();
+        ArrayList<String> ingredientsList = new ArrayList<String>();
+
         try {
             JSONObject sandwichDetails = new JSONObject(json);
 
             JSONObject name = sandwichDetails.getJSONObject("name");
             finalSandwich.setMainName(name.getString("mainName"));
 
-            //TODO: Also known as
+            JSONArray aka = (JSONArray) name.get("alsoKnownAs");
+            for (int i=0; i< aka.length(); i++){
+                akaList.add((String)aka.get(i));
+            }
 
             finalSandwich.setPlaceOfOrigin(sandwichDetails.getString("placeOfOrigin"));
 
@@ -27,8 +37,12 @@ public class JsonUtils {
 
             finalSandwich.setImage(sandwichDetails.getString("image"));
 
-            //TODO: Ingredience
+            JSONArray ingredients = (JSONArray) sandwichDetails.get("ingredients");
+            for (int i=0; i< ingredients.length(); i++){
+                ingredientsList.add((String)ingredients.get(i));
+            }
 
+            return finalSandwich;
 
         } catch (final JSONException e) {
             Log.e(TAG, "JSON parsing error: ");
